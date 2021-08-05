b8e9c0ddd1b0987ab7a099949cacbdf5176e703b
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-06 05:12:06 +0900

    renew

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,38 @@ import React, {useState,useEffect} from "react";
 import Image from "next/image";
 import fav from "../public/DSCF0026.JPG";
 import Axios from "axios";
+//import pg from "pg";
+var {Client}=require("pg");
+var client=new Client({
+	user:"postgres",
+	host:"13.124.176.157",
+	database:"test",
+	password:"123456",
+	port:"5432"
+});
 
+function getData(){
+	client.connect();
+	var q1="select * from member;";
+	return new Promise((resolve,rej)=>{
+		client.query(
+			q1,
+			(err,res)=>{
+				if(err){
+					dir(err);
+					return;
+				}		
+				resolve(res);
+				client.end();
+			}
+		);		
+	});
+	
+};
 export async function getStaticProps(){
 	//var data=await Axios.get("http://mnemosyne.co.kr/api/hello");
+	var data=await getData();
+	console.dir(data);
 	return {
 		props:{
 			name:"jaehyunlee"
