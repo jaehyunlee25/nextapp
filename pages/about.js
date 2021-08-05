14819f32626d98438213b4091d9d6a8d3305675b
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-06 03:14:51 +0900

    renew

--- pages/about.js
+++ pages/about.js
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import fav from "../public/DSCF0026.JPG";
 
-var [number, setNum]=React.useState(0);
-const app=()=><div>
-	about.js
-	<div>
-		<Image src={fav} width={100} height={100} alt="fav" />
+const app=()=>{	
+	var [number, setNum]=React.useState(0);
+	return <div>
+		about.js
+		<div>
+			<Image src={fav} width={100} height={100} alt="fav" />
+		</div>
+		<div>{number}</div>
 	</div>
-	<div>{number}</div>
-</div>
+};
 export default app;
