ea03b4fd32e8e1c7fd6d1748208c7f2103767658
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-06 02:40:23 +0900

    renew

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+//모든 페이지의 공통 레이아웃을 정의한다.
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+	<>
+		<h1>mnemosyne.co.kr</h1>
+		<Component {...pageProps} />
+	<>
 }
 
 export default MyApp
