64c136f38c671f4fdec9b7bc3c85a5b11304948b
jae hyun lee <jaehyunlee25@gmail.com>
2021-08-05 07:08:06 +0000

    first

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+import fav from "../public/favicon.ico";
+const app=()=><div>
+	about.js
+	<Image src={fav} alt="fav" />
+</div>
+export default app;
