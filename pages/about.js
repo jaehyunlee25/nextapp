45fa13c7a8ac01a85d5d4fba4f066df99293a934
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-07 07:30:55 +0900

    renew

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,10 @@
 import React, {useState,useEffect} from "react";
-import Image from "next/image";
+//import Image from "next/image";
 import fav from "../public/DSCF0026.JPG";
 import Axios from "axios";
-//import pg from "pg";
-var {Client}=require("pg");
-var client=new Client({
-	user:"postgres",
-	host:"13.124.176.157",
-	database:"test",
-	password:"123456",
-	port:"5432"
-});
-
-function getData(){
-	client.connect();
-	var q1="select * from member;";
-	return new Promise((resolve,rej)=>{
-		client.query(
-			q1,
-			(err,res)=>{
-				if(err){
-					dir(err);
-					return;
-				}		
-				resolve(res);
-				client.end();
-			}
-		);		
-	});
-	
-};
 export async function getStaticProps(){
-	//var data=await Axios.get("http://mnemosyne.co.kr/api/hello");
-	var data=await getData();
+	var data=await Axios.get("http://mnemosyne.co.kr:1000/api/hello");
+	//var data=await getData();
 	var result=data.rows.map(ob=>{
 		return {id:ob.id,name:ob.nickname}
 	});
@@ -56,10 +28,9 @@ const App=(props)=>{
 	};
 	return (
 		<div>
-			about.js
-			<div>
+			/* <div>
 				<Image src={fav} width={100} height={100} alt="fav" />
-			</div>
+			</div> */
 			{
 				list.map(ob=>{
 					return <div key={ob.id}>{ob.name}</div>
