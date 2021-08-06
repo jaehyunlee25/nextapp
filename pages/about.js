04a4f93e733a9eeeb4a20da3932efc774ee78e04
jaehyunlee <jaehyunlee25@gmail.com>
2021-08-07 07:58:03 +0900

    renew

--- pages/about.js
+++ pages/about.js
@@ -25,9 +25,7 @@ const App=(props)=>{
 			{
 				list.map((ar,i)=>{
 					return <tr key={i}>
-						ar.map((item,j)=>{
-							return <td key={j}>{item}</td>							
-						});
+						ar.map((item,j)=><td key={j}>{item}</td>);
 					</tr>
 				})				
 			}
